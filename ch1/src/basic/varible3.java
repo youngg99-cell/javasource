40a99489bd67ca8c2000be175ac3e6ac0f10662d
leeseoyoung <youngg99@bau.ac.kr>
2025-09-23 17:59:17 +0900

    변수 정리

--- ch1/src/basic/varible3.java
+++ ch1/src/basic/varible3.java
@@ -4,17 +4,15 @@ public class varible3 {
     public static void main(String[] args) {
         // 실수형 : float, double (기본타입)
         // float Source1 = 50;
-        float Source1 = 50.15f;
         // float :F,f
-        double Source2 = 45.125d;
-        // double : D,d
+        float score1 = 50.15f;
+        // double : d,D
+        double score2 = 45.125d;
         System.out.println("score1 = " + score1); // 50.0
         System.out.println("score2 = " + score2); // 45.0
         // %10.2f : %전체자릿수.소수점자릿수
-        System.out.printf(format:"score1 = %10.2f, score2= %f\n", score1,score2); //45
-         System.out.printf(format:"score1 = %.2f, score2= %f\n", score1,score2); //45
-        
-       
-        
+        System.out.printf("score1 = %10.2f, score2 = %f\n", score1, score2); // 45.0
+        System.out.printf("score1 = %.2f, score2 = %f\n", score1, score2); // 45.0
+
     }
 }
--- ch1/src/basic/variblel.java
+++ ch1/src/basic/variblel.java
@@ -4,17 +4,17 @@ public class variblel {
     public static void main(String[] args) {
         // 변수 선언 후 값을 할당
         int num = 10;
-
         int sum = num + 20;
         // + :연결
         System.out.println("num 변수 값" + num);
         System.out.println("num + 20 =" + sum);
-        System.out.printf(format:"num 변수 값: %d\n",num);
+        System.out.printf("num 변수 값 : %d\n", num);
         // num 변수 값 : 10age=25,maxSpeed=10
 
         int age = 25, maxSpeed = 10;
         System.out.println("age=" + age + ", maxSpeed=" + maxSpeed);
-        System.out.printf(format:"age = %d"maxSpeed = %d ?num);
+        System.out.printf("age = %d, maxSpeed = %d\n", age, maxSpeed);
+        System.out.printf("age = %10d, maxSpeed = %5d\n", age, maxSpeed);
 
         num = 35;
         // 상수 선언과 할당
